fcb8fbb1743885a5154bee7b9ec4a3c46718a211
Matthew Lord <matty.j.lord@gmail.com>
2025-03-21 17:17:47 +1000

    fix: eslint errors

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import { Logo } from "../common/logo";
-import { FaPhoneAlt } from "react-icons/fa";
 import StartFreeButton from "../common/start-free-button";
 import Link from "next/link";
 
