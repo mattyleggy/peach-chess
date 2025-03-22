7584d1afedf682e390d4ca012bff0da46774d7b2
Matthew Lord <matty.j.lord@gmail.com>
2025-03-22 20:24:45 +1000

    feat: add in video for ben

--- components/common/play-button.tsx
+++ components/common/play-button.tsx
@@ -1,9 +1,31 @@
-import { FaArrowRight } from "react-icons/fa6";
+"use client";
+
+import { FaPlay } from "react-icons/fa6";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useState } from "react";
+import { DialogTitle } from "@radix-ui/react-dialog";
 
 export const PlayButton = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="w-24 h-24 lg:w-32 lg:h-32 rounded-full bg-secondary flex items-center justify-center cursor-pointer hover:bg-primary transition-colors border-8 border-white">
-            <FaArrowRight className="w-8 h-8 text-white" />
-        </div>
+        <>
+            <div
+                onClick={() => setIsOpen(true)}
+                className="w-24 h-24 lg:w-32 lg:h-32 rounded-full bg-secondary flex items-center justify-center cursor-pointer hover:bg-primary transition-colors border-8 border-white"
+            >
+                <FaPlay className="w-8 h-8 text-white" />
+            </div>
+
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                <DialogTitle></DialogTitle>
+                <DialogContent className="h-full max-h-[90vh] w-full max-w-4xl ">
+                    <video controls className="h-full aspect-video bg-black">
+                        <source src="video/bp.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 };
