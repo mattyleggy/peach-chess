9c68c63e96d8c7bf0e7dd5db7f5d2764eed6c61d
Matthew Lord <matty.j.lord@gmail.com>
2025-05-05 20:20:46 +1000

    fix: eslint errors

--- components/sections/who-are-we.tsx
+++ components/sections/who-are-we.tsx
@@ -59,7 +59,7 @@ export default function WhoAreWe({ detailed = false }: WhoAreWeProps) {
                             <Typography variant="h3">My Current Focus</Typography>
                             <p className="text-muted-foreground mt-4">
                                 After over a decade of teaching chess, I discovered I have profound
-                                passion for teaching. I'm due to finish my degree in primary education at the end of 2025 as I am dedicated to teaching, inspiring and supporting the growth of other
+                                passion for teaching. I&apos;m due to finish my degree in primary education at the end of 2025 as I am dedicated to teaching, inspiring and supporting the growth of other
                                 chess players. In addition to my teaching endeavours, I run school
                                 chess tournaments for the Toowoomba Chess Club, striving to promote
                                 and strengthen chess in the region. Equipped with extensive chess
