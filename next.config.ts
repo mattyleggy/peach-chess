3ee71118b7d3555a3bd61cc37675fd60d651d904
Matthew Lord <matty.j.lord@gmail.com>
2025-05-22 15:06:58 +1000

    feat: add in redirect if non-www

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async redirects() {
+        return [
+            {
+                source: "/:path*",
+                has: [
+                    {
+                        type: "host",
+                        value: "peachchess.com.au",
+                    },
+                ],
+                destination: "https://www.peachchess.com.au/:path*",
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
